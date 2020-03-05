8b99d2e113645d52fe9de978dd33d08a569d2555
Charles Ivia <iviacharles@gmail.com>
2020-03-05 13:52:29 +0300

    Completed exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add (a, b)
+    a >= 0 && b > 0
+    a + b
+end
+
+def subtract (a, b)
+    a - b
+end
+
+def sum empty_array
+    empty_array.sum
+end
+
+def sum numbers
+    numbers.sum
+end
+
+def multiply numbers
+    product = 1
+    numbers.each do |num|
+        product = num * product
+    end
+    product
+end
+
+def power (a, b)
+    a ** b
+end
+
+def factorial number
+    product = 1
+    while number > 0
+        product = product * number
+        number -= 1
+    end 
+    product
+end
